2a08fa0e2be997e34b0624f7489c3f44f76591ee
potusurekhampha <potusurekhampha@ip-172-31-85-10.ec2.internal>
2022-01-17 16:51:22 +0000

    practiceprojectValidationofemail

---
+++ practiceprojectValidationOfEmail/src/practiceprojectValidationOfEmail/ValidationOfEmail.java
@@ -0,0 +1,51 @@
+package practiceprojectValidationOfEmail;
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class ValidationOfEmail {
+	
+	
+		   private static Scanner sc;
+		public static boolean isValidEmail(String email) {
+			   String regex = "^(.+)@(.+)$";
+			 //initialize the Pattern object
+		       Pattern pattern = Pattern.compile(regex);
+		       Matcher matcher = pattern.matcher(email);
+		       return matcher.matches();
+
+		       
+		   }
+		    public static void main(String[] args) {
+		       List<String> emails = new ArrayList<String>();
+		       // valid email addresses
+		       emails.add("tejuk@gmail.com");
+		       emails.add("chinni@gmail.com");
+		       emails.add("vaishu@example.me.org");
+		       //invalid email addresses
+		       emails.add("anu.example.com");
+		       emails.add("avi@example.com");
+		       emails.add("hari@.example.com");
+
+		       for (String value : emails) {
+		           System.out.println("The Email address " + value + " is " + (isValidEmail(value) ? "valid" : "invalid"));
+		       }
+		       
+		       System.out.println("Enter any email address to check");
+		       sc = new Scanner(System.in);
+		       String input = sc.nextLine();
+		       System.out.println("The Email address " + input + " is " + (isValidEmail(input) ? "valid" : "invalid"));
+		       
+		   
+		   
+}
+
+}
